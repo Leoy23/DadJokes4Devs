97ec123f630c5f36cab23a2e9c9d267b0fb172a1
Victoria Fields <victoriaashleyfields@gmail.com>
2022-11-01 18:21:38 -0600

    Create separate apiCall file with functional GET fetch;

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,15 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
 import {Joke} from '../../model';
+import {getJokes} from '../../apiCalls'
 
 export default function App() {
 
   const [joke, setJoke] = useState<Joke | {}>({})
   const [error, setError] = useState<string>('')
 
-  const fetchData = () => {
-    return fetch('https://icanhazdadjoke.com/', {
-      method: 'GET',
-      headers: {
-        "Accept" : "application/json"
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`${response.status}`)
-        }
-        return response.json()
-      })
-  }
-
   useEffect(() => {
-      fetchData()
+      getJokes()
       .then(randomJoke => setJoke(randomJoke))
       .catch(error => setError(`Uh oh, that's a ${error.message}! Try again later.`))
   }, [])
